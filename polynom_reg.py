dcf4504662363b84fe7942ce95859ca0bf892350
azet24 <148684615+azet24@users.noreply.github.com>
2023-11-20 13:59:14 +0100

    Update polynom_reg.py

--- polynom_reg.py
+++ polynom_reg.py
@@ -1,13 +1,15 @@
+#This is a project of implementing polynomial regression using expended space of features of diffrent monomials and then implementing linear regression
+
 import torch
 import matplotlib.pyplot as plt
 
 def gen_monoms(deg, x, y):
     List = []
-    for i in range(deg + 1):  # Zmieniamy na deg + 1
+    for i in range(deg + 1):  # Chanche for deg + 1
         for j in range(i + 1):
             List.append(x ** (i - j) * y ** j)
     List = torch.stack(List, dim=2)
-    return List  # Zwracamy listę tensorów
+    return List  # Return list of tensors
 
 class RegressionModel(torch.nn.Module):
     def __init__(self, n=2):
@@ -22,20 +24,20 @@ class RegressionModel(torch.nn.Module):
         return output
 
 dtype = torch.float
-device = 'cuda:0'  # Używamy GPU do przyspieszenia obliczeń
+device = 'cuda:0'  # Use GPU
 PAR = 9
 
 
-# Tworzenie dwuwymiarowego tensora
-x = torch.linspace(-1.4, 1.4, steps=64, device=device)  # Tworzymy liniową przestrzeń od -1 do 1 z 64 krokami
+# Creating 2D tensor
+x = torch.linspace(-1.4, 1.4, steps=64, device=device)
 y = torch.linspace(-1.4, 1.4, steps=64, device=device)
-xx, yy = torch.meshgrid(x, y, indexing='ij')  # Tworzymy siatkę współrzędnych
+xx, yy = torch.meshgrid(x, y, indexing='ij')  # Creating grid of coordinates
 
 # Definiowanie funkcji
-w = torch.FloatTensor(xx.shape).uniform_(-0.1, 0.1).to(device)  # Losowe szumy
-f = torch.sin(xx**2 + yy**2)**2 + w  # Funkcja, którą chcemy aproksymować
+w = torch.FloatTensor(xx.shape).uniform_(-0.1, 0.1).to(device)  # Creating random noise
+f = torch.sin(xx**2 + yy**2)**2 + w  # Creating data for approximation
 
-torch.manual_seed(42)  # Ustawiamy ziarno losowości dla powtarzalności wyników
+torch.manual_seed(42)  # Setting random seed
 X = torch.stack((xx, yy), dim=2)
 X = X.to(device) 
 model = RegressionModel(PAR)
@@ -53,25 +55,25 @@ for epoch in epochs:
     if epoch % 50 == 49:  
         print(f"Epoch {epoch} | Loss: {loss}")  
 
-# Przewidywanie modelu
+# Model predictions
 model.eval()  
 with torch.no_grad():  
     predictions = model(X)  
 
-# Przygotowanie danych do wykresu
+# Preparing data for plot
 x = X[:, :, 0].cpu().numpy()
 y = X[:, :, 1].cpu().numpy()
 z_real = f.cpu().numpy()
-z_pred = predictions.squeeze(-1).cpu().numpy()  # Spłaszczamy ostatni wymiar
+z_pred = predictions.squeeze(-1).cpu().numpy()  # Squeeze last dimension of tensor
 
-# Tworzenie wykresu
+# Creating plot
 fig = plt.figure(figsize=(12, 6))  
 ax = fig.add_subplot(111, projection='3d')  
 
-# Wykres dla oryginalnych danych
+# Plot for input data
 scatter = ax.scatter(x, y, z_real, color='red', s=.7, alpha=0.3)  
 
-# Wykres dla przewidywań modelu
+# Plot for predictions
 surface = ax.plot_surface(x, y, z_pred, color='blue', alpha=0.4)  
 
 ax.set_title('Dane wejściowe i przewidywania modelu') 
